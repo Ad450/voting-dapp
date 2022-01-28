dcb354312d004ccd71a9ca33604a9a5c0edded6f
Ad450 <adjeimanue@gmail.com>
2022-01-28 14:43:25 +0000

    voting smart contract - voter contract underway

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,52 +1,53 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.0 <0.9.0;
 
-contract Voting {
-    uint256  teamA = 0;
-    uint256 teamB = 0;
-    uint256 teamC = 0;
-    address voter;
+contract Voter{
+    // this represents a single votes
+   address public voter;
 
-    mapping(address=> uint) eligibility;
+    // make sure the voter is actually the bearer of the account 
+    // just like checking the voterID of a person before voting
+   constructor() {
+       voter = msg.sender;
+   }
+    // create an event that will be listened to on the frontend when a user votes and when a user tries to overvote
+    event Voted(address who);
 
-    constructor () public {
-        voter = msg.sender;
-    }
+   // create an error that will be sent to potential voter when overvoting is tried
+    error OverVoting (string message);
 
-    modifier onlyEligible{
-        require(eligibility[voter] == 0, "you can vote only once");
+    // vote modifier checks if address (voter) has already voted 
+    mapping (address => uint16) private count;
+
+    //vote modifier checks if address (voter) has already voted 
+    modifier canVote{
+        require(count[voter] >= 0, 'vote counts cannot be negative ');
+        require(count[voter] == 1 && voter == msg.sender, 'must be a user to perform this operation');
         _;
     }
 
-    function voteForA()  private onlyEligible returns (uint){
-        uint votes = teamA + 1;
-        eligibility[voter] = 1;
-        return votes;
+    // vote function to carry out voting
+
+    function vote() canVote public view{
+        // further checks for overvoting
+        if(count[voter] == 0){
+            count[voter] + 1;
+        }
     }
 
-    function voteForB() private onlyEligible returns (uint){
-        uint votes = teamB + 1;
-        eligibility[voter] = 1;
-        return votes;
-    }
-
-    function voteForC() private onlyEligible returns (uint){
-         uint votes = teamC + 1;
-         eligibility[voter] = 1;
-         return votes;
-    }
-
-    function returnA() public  returns (uint){
-       return voteForA();
-    }
-    function returnB() public   returns (uint){
-       return voteForB();
-    }
-    function returnC() public  returns (uint){
-       return voteForC();
-    }
 }
 
 
 
 
+
+
+/**  idea for voting
+ for a user
+ a user can vote for either one of three parties
+ a user can only invoke the vote functions once
+
+ for vote counter
+ the vote counter will be a contract that keeps track of all votes..
+
+ **/
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Voter = artifacts.require("Voter");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Voter);
 };
