83b9542d446f6da4dd32bb58349a99841243c68c
Ad450 <adjeimanue@gmail.com>
2022-02-05 23:23:32 +0000

    getPartyVotes from web3 working

--- app/index.js
+++ app/index.js
@@ -1,6 +1,40 @@
 // require web3 into the project
 const Web3 = require ('web3');
 
-// create an instance of web3 passing in a provider
-const web3 = new Web3(new Web3.providers.WebSocketProvider(''));
-console.log(Web3);
+// require ElectoralCommission contract abi into app.js
+// the abi exposes all the functions and variables we can communicate with in the contract
+const contractJSON = require('../build/contracts/ElectoralCommission.json');
+
+// abi
+const initContract = async()=>{
+
+    const abi = contractJSON.abi;
+    
+    const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+    // to get address from network
+    const id = await web3.eth.net.getId();
+    const address = contractJSON.networks[id].address;
+    
+    // create an instance of web3 passing in a provider
+    
+   // console.log(web3);
+
+    const contract = new web3.eth.Contract(abi, address);
+    return contract;
+}
+
+initContract();
+
+const getPartyVotes = async(party)=>{
+    const electoralCommission = await initContract();
+    try {       
+       const result = await electoralCommission.methods.getPartyVotes(party).call();
+       console.log(result);
+       
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+getPartyVotes('A');
