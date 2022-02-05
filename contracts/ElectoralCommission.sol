d662b28d5120346a61cb09f53426d05cdbe014b6
Ad450 <adjeimanue@gmail.com>
2022-02-05 15:21:27 +0000

    refactored contract

--- contracts/ElectoralCommission.sol
+++ contracts/ElectoralCommission.sol
@@ -1,68 +1,75 @@
+pragma solidity ^0.5.16;
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.0 <0.9.0;
 
-contract ElectoralCommission{
+
+contract ElectoralCommission {
+
+    address public owner;
 
     // declare a voter object which contains the information of each citizen 
-   struct Voter{
-       address immutable voterId;
-       uint immutable vote;
-       bool immutable hasVoted;
-   }
+    struct Voter {
+        address voterId;
+        uint vote;
+        bool hasVoted;
+    }
 
    // declare a ballot object that contains the number of votes in the election process
-   struct BallotBox{
-       uint immutable voteCount;
-   }
-
-  // party object
-   struct Party{
-       uint immutable totalVotes;
-   }
+    struct BallotBox {
+        uint voteCount;
+    }
 
    // various parties involved in the election
-   Party partyA;
-   Party partB;
-   Party partC;
+   // hard coded parties for testing
+    string public partyA;
+    string public partyB;
+    string public partyC;
 
    // map all parties to corresponding votes
-   mapping  (Party => uint) partyVotes; 
+    mapping (string => uint) public partyVotes; 
 
-   // hasAlreadyVotedError will be thrown when user tries to vote again
-   error HasAlreadyVotedError (address who);
+   // hasAlreadyVotedError will be thrown when user triespublic to vote again
+
+    //error HasAlreadyVotedError (string message);
+    bool public triggerVoteCountError = false;
 
    // events to used by the UI
    // this emits the voted event containing the address of the voter
-   event Voted(address who, string message);
+    event Voted(address who, string message);
+    
 
     // creating our one object of voter and ballotBox
-     Voter voter;
-     BallotBox ballotBox;
+    Voter public voter;
+    BallotBox public ballotBox;
 
    // ensure the address calling this smart contract is the voter
    // set vote and hasVoted as seen below
-   constructor ()public{
-      voter =  Voter({voterId : msg.sender, vote : 0, hasVoted:false});
-   }
+    constructor () public{
+        voter =  Voter({voterId : msg.sender, vote : 0, hasVoted:false});
+        // hard coded parties for testing
+        partyA = 'A';
+        partyB = 'B';
+        partyC = 'C';
+}
 
-   // ensure the voter is the caller of this contract
-   modifier verifyVoter{
+// ensure the voter is the caller of this contract
+    modifier verifyVoter{
        require(!(voter.hasVoted) && voter.voterId == msg.sender && voter.vote == 0);
        _;
    }
 
-    // read all votes 
+    // read total votes in election 
     function getTotalVoteCast() public view returns (uint){
         return ballotBox.voteCount;        
     }
 
-    function getPartyVotes(Party _party) public view returns (uint){
-        return partyVotes[_party]       
+    // read total votes of a party
+    function getPartyVotes(string memory _party) public view returns (uint){
+        return partyVotes[_party];     
     }
 
 
     // all verified voters can vote
-    function voteForParty(Party _party) private verifyVoter {
+    function voteForParty(string memory _party) public verifyVoter {
         if(!triggerVoteCountError){
 
         // update the voter info
@@ -70,7 +77,7 @@ contract ElectoralCommission{
         voter.hasVoted = true;
 
         //increase the party total votest by one
-        partyVotes[_party] =+ 1;
+        partyVotes[_party] += 1;
         
         // set voteCountError to true to be able to revert network if voter tries to vote again
         triggerVoteCountError = true;
@@ -78,42 +85,12 @@ contract ElectoralCommission{
         // increase the votes in the ballotBox
         ballotBox.voteCount += 1;
 
-        emit Voted({who: voter.voterId});
+        emit Voted({who: voter.voterId , message: 'Thank you, vote recorded successfuly'});
         }else{
-            revert HasAlreadyVotedError({who : voter.adress, message : "You have already voted, thank you" });
+            revert ('You have already voted');
         }
         
     } 
 
 }
 
-
-
-
-
-/**  idea for voting
- for a user
- a user can vote for either one of three parties
- a user can only invoke the vote functions once
-
- for vote counter
- the vote counter will be a contract that keeps track of all votes..
-
- **/
-
- /**
- The idea is to to do the work of the electoral commission using a smart contract
- Lets first find out what the EC does
-
- primary functions of the EC
-
- 1. to validate all citizens prior to casting process
- 2. Ensure no citizen votes more than once
- 3. counts all the votes
- 4. Declare winner
-
- ......................
- the electoral commission contract will have three objects to keep track of
- the voter, the number of in the ballot box
-
-  **/
