d82abaa6e89d9f7924a6c725d1e7a42512e24e05
Ad450 <adjeimanue@gmail.com>
2022-02-08 18:26:57 +0000

    minor refactoring

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -2,15 +2,26 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
+  Text,
+  Flex,
   theme,
+  VStack,
+  
 } from "@chakra-ui/react"
 
 
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      
-    </Box>
+    <VStack >
+    <Flex>
+     <Box height='container.xl'bg='pink.200' width='container.xl'>
+      <Box paddingTop='50'>
+        <Text>hello Ghana</Text>
+      </Box>
+     </Box>
+      </Flex>
+    </VStack>
+   
   </ChakraProvider>
 )
---
+++ src/src/features/voter_verification/components/verify_voter.tsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+function VerifyVoter(){
+    return <>
+    
+    </>
+}
+
+export {VerifyVoter};
