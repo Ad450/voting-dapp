d2591c5860db2fbc48940d958bfb21a2dc05dc05
Ad450 <adjeimanue@gmail.com>
2022-02-08 21:19:24 +0000

    verify voter form setup

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,27 +1,30 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
-  Text,
-  Flex,
   theme,
+  Square,
+  Heading,
   VStack,
-  
-} from "@chakra-ui/react"
-
-
+} from "@chakra-ui/react";
+import { VerifyVoter } from "./features/voter_verification/components/verify_voter";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <VStack >
-    <Flex>
-     <Box height='container.xl'bg='pink.200' width='container.xl'>
-      <Box paddingTop='50'>
-        <Text>hello Ghana</Text>
+    <Box height="container.xl" bg="pink.200" p="0">
+      <Box paddingTop="50">
+        <VStack>
+          <Heading isTruncated as="h2" size="2xl">
+            {" "}
+            Let's Verify your Email!!{" "}
+          </Heading>
+          <Box paddingTop="100">
+            <Square minWidth="container.sm">
+              <VerifyVoter></VerifyVoter>
+            </Square>
+          </Box>
+        </VStack>
       </Box>
-     </Box>
-      </Flex>
-    </VStack>
-   
+    </Box>
   </ChakraProvider>
-)
+);
--- src/src/features/voter_verification/components/verify_voter.tsx
+++ src/src/features/voter_verification/components/verify_voter.tsx
@@ -1,10 +1,53 @@
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  PinInput,
+  PinInputField,
+  Stack,
+  HStack,
+  Box,
+  Button,
+} from "@chakra-ui/react";
 
-import React from "react";
+const VerifyVoter = () => {
+  return (
+    <Stack>
+      <FormControl>
+        <FormLabel htmlFor="email">Email address</FormLabel>
+        <Input id="email" type="email" />
+        <FormHelperText>We'll never share your email.</FormHelperText>
+      </FormControl>
+      <CustomPinField></CustomPinField>
+      {
+        // will replace sent Otp with verify otp once the user has received the otp
 
-function VerifyVoter(){
-    return <>
-    
-    </>
-}
+        <Button colorScheme="red" variant="outline">
+          Send Otp
+        </Button>
+      }
+    </Stack>
+  );
+};
 
-export {VerifyVoter};
+const CustomPinField = () => {
+  return (
+    <HStack>
+      <Box paddingTop="30">
+        <PinInput otp onComplete={performVerification}>
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+          <PinInputField />
+        </PinInput>
+      </Box>
+    </HStack>
+  );
+};
+
+// this will verify pin
+const performVerification = () => {};
+export { VerifyVoter };
