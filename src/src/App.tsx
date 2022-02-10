7a3d0aba03d9ccf990e9bdc548be6134480bbb28
Ad450 <adjeimanue@gmail.com>
2022-02-10 16:14:24 +0000

    react router added. UI goes blank. Needs fixing asap

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { ChakraProvider, Box, theme, Heading, VStack } from "@chakra-ui/react";
 import { VerifyVoter } from "./features/voter_verification/components/verify_voter";
+import { Routes, Route } from "react-router-dom";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <VerifyVoter></VerifyVoter>
+    <Routes>
+      <Route path="/" element={VerifyVoter} />
+    </Routes>
   </ChakraProvider>
 );
