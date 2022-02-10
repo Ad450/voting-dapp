92f4b241a8ba5a0fa0d4b65f8576290bead3100f
Ad450 <adjeimanue@gmail.com>
2022-02-10 09:16:09 +0000

    minor changes

--- src/src/core/utils/validators.ts
+++ src/src/core/utils/validators.ts
@@ -1,12 +1,12 @@
 abstract class Validator {
-    static reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  static regex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   static validateEmail(email :string):boolean{
     if(email === '' || email === null){
       return false;
     }
 
-    return  this.reg.test(email);
+    return  this.regex.test(email);
   }
 }
 
