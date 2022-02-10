2351497a7acae601c29376f9c3b73a143693c1d1
Ad450 <adjeimanue@gmail.com>
2022-02-10 21:27:57 +0000

    testing react routing

---
+++ src/src/features/vote/components/voting.tsx
@@ -0,0 +1,7 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export const Voting = () => (
+  <Flex height="100vh" alignItems="center" justifyContent="center">
+    <Text>Voting....</Text>
+  </Flex>
+);
