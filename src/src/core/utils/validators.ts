c5c297355116fd4577310c01cedddeccbaa0b2f8
Ad450 <adjeimanue@gmail.com>
2022-02-10 10:39:40 +0000

    changed regex

--- src/src/core/utils/validators.ts
+++ src/src/core/utils/validators.ts
@@ -1,5 +1,5 @@
 abstract class Validator {
-  static regex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  static regex: RegExp =  /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
   static validateEmail(email :string):boolean{
     if(email === '' || email === null){
